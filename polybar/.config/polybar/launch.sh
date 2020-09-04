78fb1ac120670d7f7f2027156e004e3145a90dec
Christoffer Moesgaard <christoffer@magenta.dk>
2020-09-04 11:10:29 +0200

    Update polybar launch script
    First attempt at making a system-agnostic launch script for arbitrary
    screen setups

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,6 +7,18 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log
-polybar main >>/tmp/polybar.log 2>&1 &
+
+PRIMARY=$(xrandr | grep ' connected' | grep 'primary' | cut -d' ' -f1)
+SECONDARY=$(xrandr | grep ' connected' | grep -v 'primary' | cut -d' ' -f1)
+
+echo Lauching primary bar on $PRIMARY
+echo Lauching secondary bar on $SECONDARY
+
+MONITOR=$PRIMARY polybar main >>/tmp/polybar.log 2>&1 &
+
+for MONITOR in $SECONDARY
+do
+        MONITOR=$MONITOR polybar secondary >>/tmp/polybar.log 2>&1 &
+done
 
 echo "Bars launched..."
