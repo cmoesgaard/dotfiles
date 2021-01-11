740b2c1531e71ed638a0bb707d9f4ab94e47efc4
Christoffer Moesgaard <cmoesgaard@gmail.com>
2021-01-11 21:25:14 +0100

    Add zoop-rofi config 👉😎👉

---
+++ zoop-rofi/.config/zoop-rofi/entries.lisp
@@ -0,0 +1,7 @@
+(
+ ((name . "👉😎👉") (aliases "zoop" "fingerguns"))
+ ((name . "¯\\_(ツ)_/¯") (aliases "shrug"))
+ ((name . "(╯°□°）╯︵ ┻━┻") (aliases "tableflip"))
+ ((name . "┬──┬ ノ( ゜-゜ノ)") (aliases "unflip"))
+ ((name . "ʕ•ᴥ•ʔ") (aliases "bear"))
+)
