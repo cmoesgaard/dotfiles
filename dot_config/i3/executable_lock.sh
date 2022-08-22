68a08354f4ee53e7816ff44fd35b8a4ede3fa20f
Christoffer Moesgaard <cmo@corti.ai>
2022-08-22 15:07:11 +0200

    Update i3 config
    Add more startup apps
    Update lock config

---
+++ dot_config/i3/executable_lock.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+i3lock -k -c 2E3440 --insidever-color=A3BE8C --insidewrong-color=BF616A --inside-color=3B4252 --ringver-color=D8DEE9 --ringwrong-color=D8DEE9 --ring-color=4C566A --line-uses-inside --keyhl-color=81A1C1 --bshl-color=BF616A --separator-color=434C5E --time-color=D8DEE9 --date-color=D8DEE9 --verif-text="" --wrong-text="" --noinput-text="" --ring-width=15 --radius=120
+
