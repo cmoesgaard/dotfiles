93f07e0b58c85139e5fc65c6fa3049437b2314ba
Christoffer Moesgaard <cmo@corti.ai>
2023-01-18 10:30:40 +0100

    Update fish config
    Autostart ssh_agent

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,3 +1,10 @@
 starship init fish | source
 zoxide init fish | source
 set fish_greeting
+
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
